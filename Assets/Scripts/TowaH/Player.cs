60ea847520aecf2ceb096029c5ac5503ca52394a
Chaika9 <chaika9.perso@gmail.com>
2023-03-26 17:49:01 +0200

    Fix

--- Assets/Scripts/TowaH/Player.cs
+++ Assets/Scripts/TowaH/Player.cs
@@ -4,13 +4,10 @@ namespace TowaH {
     public class Player : MonoBehaviour {
         [SerializeField] private Vector3 spawnBlockPosition;
         [SerializeField] private Quaternion spawnBlockRotation;
-        
-        private PlayerInfo playerInfo;
 
         public void SpawnRandomBlock() {
             Debug.Log("SpawnRandomBlock");
-            Debug.Assert(playerInfo != null, "playerConnection != null");
-            
+
             int whichItem = Random.Range(0, TowaHGameManager.instance.AvailableBlockPrefabs.Length);
             GameObject blockPrefab = TowaHGameManager.instance.AvailableBlockPrefabs[whichItem];
             
